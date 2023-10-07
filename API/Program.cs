62d756caa9b3503361c2da70db9198ce91be289b
Ogun LM Ayaz <ogunayaz6161@gmail.com>
2023-10-07 15:53:31 +0300

    end of 5

--- API/Program.cs
+++ API/Program.cs
@@ -1,30 +1,22 @@
-
+using API.Extensions;
+using API.Middleware;
 using Core.Data;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(opt=>{
-    
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-
-}
-);
-builder.Services.AddScoped<IProductRepository,ProrductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseStatusCodePagesWithReExecute("/erros/{0}");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
